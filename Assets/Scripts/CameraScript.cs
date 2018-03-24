3d91e4a6085d70e38e99e933a563c8cea63738c1
Fuzzyundies <Eric.Jonathon.Lambert@gmail.com>
2018-03-24 15:29:03 +0000

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+
+    private float distance = 1;
+    private float speed = 0.3f;
+    private float maxRange = 6;
+    private float minRange = 3;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            rotate(true);
+        if (Input.GetKey(KeyCode.D))
+            rotate(false);
+        if (Input.GetKey(KeyCode.W))
+            zoom(true);
+        if (Input.GetKey(KeyCode.S))
+            zoom(false);
+    }
+
+    private void rotate (bool input)
+    {
+        float step = speed * Time.deltaTime;
+        float orbit = 2F * distance * Mathf.PI;
+        float distanceRadians = (speed / orbit) * 20 * Mathf.PI;
+        if (input)
+            transform.RotateAround(player.transform.position, Vector3.up, -distanceRadians);
+        else
+            transform.RotateAround(player.transform.position, Vector3.up, distanceRadians);
+    }
+
+    private void zoom (bool input)
+    {
+        Ray cRay = Camera.main.ScreenPointToRay(player.transform.position);
+        RaycastHit hit;
+        Physics.Raycast(cRay, out hit);
+
+        Debug.Log(hit.distance);
+        if (input && hit.distance >= minRange)
+            transform.Translate(0, 0, speed, Space.Self);
+        else if(!input && hit.distance <= maxRange)
+            transform.Translate(0, 0, -speed, Space.Self);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private int health;
+    private int actions;
+    private int damage;
+    private int[] actionQueue = new int[2];
+    private bool ready;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        health = 3;
+        actions = 2;
+        damage = 2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+        if (actionQueue[0] != 0 && actionQueue[1] != 0)
+            ready = true;
+        else
+            ready = false;
+	}
+
+    public void AttackOfOpporunity()
+    {
+
+    }
+
+    public void Block()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Lunge()
+    {
+
+    }
+
+    public void Advance()
+    {
+
+    }
+
+    public void Retreat()
+    {
+
+    }
+
+    public void CircleLeft()
+    {
+
+    }
+
+    public void CircleRight()
+    {
+
+    }
+
+    public void DealDamage(int damage)
+    {
+
+    }
+
+    public void RecieveDamage(int damage)
+    {
+
+    }
+}
